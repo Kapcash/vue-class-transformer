6f0207c2ee7fbf43dfc8961f904474bb56bbb960
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-02-01 21:24:10 +0100

    chore: copy required node_modules to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const deps = [
+  '@nuxtjs/eslint-config-typescript',
+  '@typescript-eslint/eslint-plugin',
+  '@typescript-eslint/parser',
+  'cli-progress',
+  'eslint',
+  'mkdirp',
+  'vue-template-compiler',
+  'yargs',
+]
+
+const dependencies = deps.map((dep) => ({ from: `node_modules/${dep}`, to: `node_modules/${dep}` }))
 
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'out.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
@@ -17,7 +31,12 @@ module.exports = {
   optimization: {
 		// We no not want to minimize our code.
 		minimize: true,
-	},
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: dependencies
+    })
+  ],
   module: {
     rules: [
       {
