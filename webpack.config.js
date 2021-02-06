d44c5b485ecb3a85bb9e75abc98d3b0fa1a302dc
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-02-06 19:44:48 +0100

    chore: ts to deps, no copy deps to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,5 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const CopyPlugin = require("copy-webpack-plugin");
-
-const deps = [
-  '@nuxtjs/eslint-config-typescript',
-  '@typescript-eslint/eslint-plugin',
-  '@typescript-eslint/parser',
-  'cli-progress',
-  'eslint',
-  'mkdirp',
-  'vue-template-compiler',
-  'yargs',
-]
-
-const dependencies = deps.map((dep) => ({ from: `node_modules/${dep}`, to: `node_modules/${dep}` }))
 
 module.exports = {
   entry: './src/index.ts',
@@ -29,14 +15,8 @@ module.exports = {
     extensions: ['.ts', '.js'],
   },
   optimization: {
-		// We no not want to minimize our code.
 		minimize: true,
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: dependencies
-    })
-  ],
   module: {
     rules: [
       {
